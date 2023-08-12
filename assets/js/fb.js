6fe08c6501873a9b95fb9653a22c9dbfed56cb08
Francisco Barrientos <fran.xd.99@gmail.com>
2023-08-12 12:02:07 -0400

    Add(modal): Añadiendo modal de imagenes de cada proyecto

--- assets/js/fb.js
+++ assets/js/fb.js
@@ -1,18 +1,3 @@
-/*!
-=========================================================
-* JohnDoe Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -31,120 +16,79 @@ $(document).ready(function(){
     });
 });
 
-// protfolio filters
-$(window).on("load", function() {
-    var t = $(".portfolio-container");
-    t.isotope({
-        filter: ".new",
-        animationOptions: {
-            duration: 750,
-            easing: "linear",
-            queue: !1
-        }
-    }), $(".filters a").click(function() {
-        $(".filters .active").removeClass("active"), $(this).addClass("active");
-        var i = $(this).attr("data-filter");
-        return t.isotope({
-            filter: i,
-            animationOptions: {
-                duration: 750,
-                easing: "linear",
-                queue: !1
-            }
-        }), !1
-    });
+document.getElementById("show-modal").addEventListener("click", function() {
+  var modal = document.getElementById("myModal");
+  var closeModal = function() {
+      modal.style.display = "none";
+  };
+
+  modal.style.display = "block";
+  modal.querySelector(".close").addEventListener("click", closeModal);
+
+  var carousel = document.querySelector(".carousel");
+  var images = document.querySelectorAll(".carousel img");
+  var currentIndex = 0;
+
+  function showImage(index) {
+      if (index < 0) {
+          currentIndex = images.length - 1;
+      } else if (index >= images.length) {
+          currentIndex = 0;
+      }
+
+      images.forEach(function(item, idx) {
+          item.style.display = idx === currentIndex ? "block" : "none";
+      });
+  }
+
+  showImage(currentIndex);
+
+  modal.querySelector(".prev").addEventListener("click", function() {
+      currentIndex--;
+      showImage(currentIndex);
+  });
+
+  modal.querySelector(".next").addEventListener("click", function() {
+      currentIndex++;
+      showImage(currentIndex);
+  });
 });
 
 
-// google maps
-function initMap() {
-// Styles a map in night mode.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
-        zoom: 12,
-        scrollwheel:  false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-      styles: [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]
-    });
-}
+document.getElementById("show-modal-2").addEventListener("click", function() {
+  var modal = document.getElementById("myModal-2");
+  var closeModal = function() {
+      modal.style.display = "none";
+  };
+
+  modal.style.display = "block";
+  modal.querySelector(".close").addEventListener("click", closeModal);
+
+  var carousel = document.querySelector("#myModal-2 .carousel"); // Cambia el selector
+  var images = document.querySelectorAll("#myModal-2 .carousel img"); // Cambia el selector
+  var currentIndex = 0;
+
+  function showImage(index) {
+      if (index < 0) {
+          currentIndex = images.length - 1;
+      } else if (index >= images.length) {
+          currentIndex = 0;
+      }
+
+      images.forEach(function(item, idx) {
+          item.style.display = idx === currentIndex ? "block" : "none";
+      });
+  }
+
+  showImage(currentIndex);
+
+  modal.querySelector(".prev").addEventListener("click", function() {
+      currentIndex--;
+      showImage(currentIndex);
+  });
+
+  modal.querySelector(".next").addEventListener("click", function() {
+      currentIndex++;
+      showImage(currentIndex);
+  });
+});
